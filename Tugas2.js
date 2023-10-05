c0d048cab50cc9ec6ec92ed163cff3172130e414
M. Hisyam Ahmad Hasan Hazmi <hisyamhazmi26@gmail.com>
2023-10-05 00:04:35 -1200

    TugasHisyam2

---
+++ Tugas2.js
@@ -0,0 +1,9 @@
+const profile = {
+    firstName: "M.Hisyam Ahmad",
+    lastName : "Hasan Hazmi",
+    age:20,
+    isMarried: false
+    
+};
+const {firstName, lastName, age, isMarried} = profile;
+console.log (`Hallo Nama Saya ${firstName} ${lastName} Umur Saya ${age} isMarried ${isMarried}`)
---
+++ sesi2.js
@@ -0,0 +1,9 @@
+const profile = {
+    fristName : "M. Hisyam Ahmad"
+    lastName : "Hasan Hazmi"
+    age : 20
+    isMarried : false
+}
+
+const{fristName,lastName,age,isMarried}=profile;
+console.log(fristName,lastName,age,isMarried)
