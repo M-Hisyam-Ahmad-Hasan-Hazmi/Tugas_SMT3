cada136229cdf50cc72454caa79714329000071e
M. Hisyam Ahmad Hasan Hazmi <hisyamhazmi26@gmail.com>
2023-10-05 00:15:14 -1200

    TugasHisyam2

--- sesi2.js
+++
@@ -1,9 +0,0 @@
-const profile = {
-    fristName : "M. Hisyam Ahmad"
-    lastName : "Hasan Hazmi"
-    age : 20
-    isMarried : false
-}
-
-const{fristName,lastName,age,isMarried}=profile;
-console.log(fristName,lastName,age,isMarried)
