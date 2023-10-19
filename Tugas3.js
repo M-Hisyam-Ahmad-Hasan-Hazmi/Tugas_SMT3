8744bb1450e50948866fb8ba0a998b9804184ac8
M. Hisyam Ahmad Hasan Hazmi <hisyamhazmi26@gmail.com>
2023-10-19 00:10:33 -1200

    TugasHisyam2

---
+++ Tugas3.js
@@ -0,0 +1,18 @@
+// Tugas 3 Nomor 1
+console.log("Tugas Nomor 1 Variable Bertipe Objek")
+
+const properti1 = { 
+ Nama : "Kedai Hisyam",
+ City : "Cicurug",
+ favoriteDrink :"Boba Coklat,Brown Sugar",
+ favoriteFood: "Mie Yamin,Seblak,Japlak",
+ isVegan : true
+
+}
+console.log(properti1)
+
+//Tugas 3 Nomor 2
+console.log("Tugas 3 Nomor 2 Variable Bertipe array")
+
+let restaurant = ["Kedai Hisyam","Cicurug","Boba Coklat, Brown Sugar","Mie Yamin,Seblak,Japlak",true]
+console.log(restaurant)
