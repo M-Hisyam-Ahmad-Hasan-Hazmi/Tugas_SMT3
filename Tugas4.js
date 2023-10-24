ab5e893259a06b5716f77213322ff04d4b1478ae
M. Hisyam Ahmad Hasan Hazmi <hisyamhazmi26@gmail.com>
2023-10-23 18:40:03 -1200

    TugasHisyam4

---
+++ Tugas4.js
@@ -0,0 +1,52 @@
+//Menggunakan if-else
+console.log ("Menggunakan if-else")
+
+var pendapatan = 600000;
+
+var uangJasa, uangKomisi;
+
+if (pendapatan <= 200000) {
+    uangJasa = 10000;
+    uangKomisi = 0.1 * pendapatan;
+} else if (pendapatan <= 500000) {
+    uangJasa = 20000;
+    uangKomisi = 0.15 * pendapatan;
+} else {
+    uangJasa = 30000;
+    uangKomisi = 0.2 * pendapatan;
+}
+var totalPembayaran = uangJasa + uangKomisi;
+
+console.log("Uang Jasa: Rp. " + uangJasa);
+console.log("Uang Komisi: Rp. " + uangKomisi);
+console.log("Total Pembayaran: Rp. " + totalPembayaran);
+
+//Switch-case 
+
+console.log("Menggunakan Switch-case")
+
+var pendapatan = 600000;
+
+var uangJasa, uangKomisi;
+
+switch (true) {
+    case pendapatan <= 200000:
+        uangJasa = 10000;
+        uangKomisi = 0.1 * pendapatan;
+        break;
+    case pendapatan <= 500000:
+        uangJasa = 20000;
+        uangKomisi = 0.15 * pendapatan;
+        break;
+    default:
+        uangJasa = 30000;
+        uangKomisi = 0.2 * pendapatan;
+}
+
+var totalPembayaran = uangJasa + uangKomisi;
+
+console.log("Uang Jasa: Rp. " + uangJasa);
+console.log("Uang Komisi: Rp. " + uangKomisi);
+console.log("Total Pembayaran: Rp. " + totalPembayaran);
+
+
